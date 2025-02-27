056dbffea6b88e1be3b3c8935696b6f3534aeb4a
Nihir14 <nihirverma14@gmail.com>
2025-02-27 14:06:35 +0530

    update

---
+++ Assignments/LeetCode Questions/Practise.java
@@ -0,0 +1,8 @@
+public class Practise {
+    public static void main(String[] args) {
+        Integer i = new Integer(10);
+        Integer j = new Integer(10);
+        System.out.println(i.equals(j));
+    }
+
+}
